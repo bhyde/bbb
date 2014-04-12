857bfad2f0b9bfb721b28736780d393e636d18fd
Ben Hyde <bhyde@pobox.com>
2014-04-12 13:39:24 -0400

    A near total rewrite triggered by finding a opkg repository I can use for emacs and svn.  Gratuitous refactoring, so it' pretty; but now it's untested.

---
+++ utils.sh
@@ -0,0 +1,32 @@
+# Local Variables:
+# mode: shell-script
+# eval: (sh-set-shell "bash")
+# End:
+
+function LOG(){ echo "LOG: $*" ; }
+
+function run-safe(){
+    trap 'LOG "leaving $(basename $0)"' EXIT
+    set -o nounset -o errexit -o verbose
+}
+
+function assure-extra-opkg-repo(){
+  R=http://feeds.angstrom-distribution.org/feeds/unstable/ipk/glibc/armv7a/base
+  if grep --silent "$R" /etc/opkg/angstrom-base.conf ; then return 0 ; fi
+  echo "src/gz angstrom-base $R" >> /etc/opkg/angstrom-base.conf
+  opkg update
+}
+
+function install-this(){
+  V=$2
+  U="$1/$V.tar.gz"
+  echo "LOG: installing $V"
+  curl --remote-name --location --insecure "$U"
+  tar xzf "$V.tar.gz"
+  rm "$V.tar.gz"
+  cd "$V"
+  ./configure
+  make install
+  cd ..
+  rm -rf "$V"
+}
